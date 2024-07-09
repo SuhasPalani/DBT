7cc58c884a06ffbabd7b3ead0e3ef74951336a89
SuhasPalani <sugipals007@gmail.com>
2024-07-08 22:50:40 -0500

    logging

--- dbtlearn/macros/logging.sql
+++ dbtlearn/macros/logging.sql
@@ -1,6 +1,7 @@
 {% macro learn_logging() %}
     {{ log("Call your mom!") }}
     {{ log("Call your dad!", info=True) }} --> Logs to the screen, too
+    { # log("Call your dad!", info=True) #} --> comments for not printing it
 --  {{ log("Call your dad!", info=True) }} --> This will be put to the screen
     {# log("Call your dad!", info=True) #} --> This won't be executed
 {% endmacro %}
